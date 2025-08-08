2720e933b963969b32b76c36f9cafddeee0e416e
Vinesh <vineshsivasubramani@gmail.com>
2025-08-08 23:42:39 +0530

    feat: add user password reset

--- app/api/routers/delivery_partner.py
+++ app/api/routers/delivery_partner.py
@@ -2,10 +2,14 @@ from typing import Annotated
 
 from app.core.security import TokenData
 from app.database.models import DeliveryPartner
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, Form, HTTPException, Request, status
 from fastapi.security import OAuth2PasswordRequestForm
 
-from app.utils import invalidate_token
+from app.config import app_settings
+
+from pydantic import EmailStr
+from app.utils import TEMPLATE_DIR, invalidate_token
+from fastapi.templating import Jinja2Templates
 
 from ..dependencies import (
     DeliveryPartnerDep,
@@ -20,6 +24,7 @@ from ..schemas.delivery_partner import (
 )
 
 router = APIRouter(prefix="/partner", tags=["Delivery Partner"])
+templates = Jinja2Templates(TEMPLATE_DIR)
 
 
 ### Register a new delivery partner
@@ -78,3 +83,41 @@ async def logout_delivery_partner(
 ):
     await invalidate_token(token_data)
     return {"detail": "Successfully logged out"}
+
+
+### Forget Password (send reset link)
+@router.get("/forgot_password", name="forgotPasswordPartner")
+async def forget_password_partner(email: EmailStr, service: DeliveryPartnerServiceDep):
+
+    await service.send_password_reset_link(email, router.prefix)
+    return {"detail": "Check email for password reset link"}
+
+
+### Reset Password Form (hidden from OpenAPI)
+@router.get("/reset_password_form", include_in_schema=False)
+async def get_reset_password_form(request: Request, token: str):
+
+    return templates.TemplateResponse(
+        request=request,
+        name="password/reset.html",
+        context={
+            "reset_url": f"http://{app_settings.APP_DOMAIN}{router.prefix}/reset_password?token={token}"
+        },
+    )
+
+
+### Reset Password (form submit)
+@router.post("/reset_password", include_in_schema=False)
+async def reset_password_partner(
+    request: Request,
+    token: str,
+    password: Annotated[str, Form()],
+    service: DeliveryPartnerServiceDep,
+):
+
+    is_success = await service.reset_password(token, password)
+
+    return templates.TemplateResponse(
+        request=request,
+        name="password/reset_success.html" if is_success else "password/reset_failed.html",
+    )
